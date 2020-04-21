77478777726891b25aac104b25bfa711cde1534e
maxBeinlich <46826147+maxBeinlich@users.noreply.github.com>
2020-04-21 18:47:08 +0200

    Level 3 completed

--- Level 3/challenge3.py
+++ Level 3/challenge3.py
@@ -43,3 +43,18 @@ solution.solution('4')
 Output:
     2
 """
+
+def solution(n):
+    n = int(n)
+    r = 0
+    while n != 1:
+        if n & 1 == 0:
+            n >>= 1
+        elif n == 3 or ((n + 1) & n) > ((n - 1) & (n - 2)):
+            n -= 1
+        else:
+            n += 1
+        r += 1
+    return r
+
+print(solution('15'))
