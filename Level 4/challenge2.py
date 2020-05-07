88a00b8ef4e81de133e16f7a94af2fdc0d01cee4
maxBeinlich <46826147+maxBeinlich@users.noreply.github.com>
2020-05-07 14:28:59 +0200

    Level 4, challenge 2 finished; Level 5, challenge 1 description

--- Level 4/challenge2.py
+++ Level 4/challenge2.py
@@ -51,3 +51,57 @@ solution.solution([300,275], [150,150], [185,100], 500)
 Output:
     9
 """
+
+import math
+from fractions import Fraction as frac
+
+def solution(dimensions, your_position, guard_position, distance):
+    res, b = {}, {}
+    h = int(round(distance / dimensions[1]))+1 # reflections in each vertical direction
+    w = int(round(distance / dimensions[0]))+1 # reflections in each horizontal direction
+
+    for i in range(-1*h, h+1, 1):
+        for j in range(-1*w, w+1, 1):
+            # generating reflected you
+            reflected_you = generate_reflection(dimensions, your_position, [j, i])
+            yBeam = get_beam(your_position, reflected_you)
+            b[yBeam[0]] = min(yBeam[1], b.get(yBeam[0], float('Inf')))
+            if res.get(yBeam[0], -1) > yBeam[1]:
+                del res[yBeam[0]]
+
+            # generating reflected guard, validating dist to your_position less than given distance
+            reflected_guard = generate_reflection(dimensions, guard_position, [j, i])
+            gBeam = get_beam(your_position, reflected_guard)
+            if gBeam[1] <= distance**2 and (gBeam[0] not in b or b[gBeam[0]] > gBeam[1]):
+                res[gBeam[0]] = min(gBeam[1], res.get(gBeam[0], float('Inf')))
+
+            # cr.update(generate_corners(dimensions, your_position, [j, i]))
+    return len(res)
+
+
+def generate_reflection(dimensions, position, reflection):
+    # calculate x position of reflection
+    x = reflection[0] * dimensions[0] 
+    if reflection[0] % 2 == 0:
+        x = x + position[0]
+    else:
+        x = x + dimensions[0] - position[0]
+    
+    # calculate y position of reflection
+    y = reflection[1] * dimensions[1] 
+    if reflection[1] % 2 == 0:
+        y = y + position[1]
+    else:
+        y = y + dimensions[1] - position[1]
+    return (x, y)
+
+def get_beam(p1, p2):
+    deltaX, deltaY = p2[0]-p1[0], p2[1]-p1[1]
+    dist_sq = deltaX**2 + deltaY**2
+    angle = frac(math.atan2(deltaX, deltaY)*180/math.pi)
+    return (angle, dist_sq)
+
+print(solution([3,2], [1,1], [2,1], 4))
+
+
+
